85725f259ddbfd45baab4aff93ca98cb836790a1
Joe Loveless <jloveless03@pm.me>
2026-06-01 16:21:10 -0500

    Delete AnsibleHTTPS.ps1

--- AnsibleHTTPS.ps1
+++
@@ -1,8 +0,0 @@
-# Generate self-signed certificate for WinRM HTTPS
-$cert = New-SelfSignedCertificate -DnsName $env:COMPUTERNAME -CertStoreLocation Cert:\LocalMachine\My
-
-# Create HTTPS listener
-winrm create winrm/config/Listener?Address=*+Transport=HTTPS "@{Hostname=`"$env:COMPUTERNAME`";CertificateThumbprint=`"$($cert.Thumbprint)`"}"
-
-# Export certificate for Ansible (if needed)
-Export-Certificate -Cert $cert -FilePath C:\winrm_cert.cer
